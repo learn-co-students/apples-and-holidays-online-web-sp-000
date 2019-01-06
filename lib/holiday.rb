37e2982feb03317eb1a76933dd45993416225597
Diana Badas <diana.badas@gmail.com>
2019-01-06 17:32:16 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,15 +59,14 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  holiday_hash.each do|season, data|
-    season["holiday_name"] = "supply_array"
-  end
-
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  winter_data = holiday_hash[:winter]
+  winter_data.values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -78,6 +77,19 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, data|
+  season = season.to_s
+  season.capitalize!
+  data.each do |attribute, value|
+    attribute = attribute.to_s
+    attribute.capitalize!
+      if attribute.include?("_")
+        attribute = attribute.split("_")
+        attribute.capitalize!
+        attribute = attribute.join(" ")
+      end
+  end
+end
 
 end
 
