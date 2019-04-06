2a891717fe688506f7e487f6b14550c8d291ca60
Ashley Miller <asm2fe@virginia.edu>
2019-04-06 20:06:50 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,7 +36,7 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-  
+
   holiday_hash[:spring][:memorial_day] << supply
 
 end
@@ -54,7 +54,7 @@ def all_winter_holiday_supplies(holiday_hash)
   list = []
   holiday_hash.each_key do |x|
     holiday_hash[x].each_key do |y|
-      binding.pry
+      #binding.pry
       holiday_hash[x][y].each do |z|
         list << z
       end
@@ -73,17 +73,30 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+  holiday_hash.each_key do |x|
+    puts "#{x.capitalize}:"
+    holiday_hash[x].each_key do |y|
+      words = false
+      if y.to_s.split("_").size > 1
+        word_array = y.to_s.split("_")
+        word_array.each {|p| p.capitalize!}
+        capital_holiday = word_array.join(" ")
+        words = true
+      end
+      puts "  #{capital_holiday}: #{holiday_hash[x][y].each_entry.to_a.join(", ")}" if words
+      puts "  #{y.capitalize}: #{holiday_hash[x][y].each_entry.to_a.join(", ")}" if !words
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  has = []
+  holiday_hash.each_key do |x|
+    holiday_hash[x].each_key do |y|
+      has << y if holiday_hash[x][y].include?("BBQ")
+    end
+  end
+  return has
 end
-
-
-
-
-
-
-
