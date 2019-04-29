ba9cace07a819907ed966905f0229ac111929fb6
Corey H <c.l.halicke@gmail.com>
2019-04-29 01:26:54 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,5 +85,15 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holidays = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holi, supplies|
+      supplies.each do |supply|
+        if supply == "BBQ"
+          holidays << holi
+        end
+      end
+    end
+  end
+  return holidays
 end
