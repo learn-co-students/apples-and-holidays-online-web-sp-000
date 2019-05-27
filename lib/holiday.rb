f94b75db3e9b0e52ab6a0ca6102a4c3eff6cf8ab
github username <github email address>
2019-05-27 16:25:34 -0700

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,9 +66,6 @@ def all_supplies_in_holidays(holiday_hash)
 
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
     bbq_holidays = []
     holiday_hash.each do |season, holiday_info|
       holiday_info.each do |holiday, supply|
