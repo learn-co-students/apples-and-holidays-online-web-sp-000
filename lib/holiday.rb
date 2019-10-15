0c36740b61f673ebb12d9392b60c205908e64537
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-15 00:54:03 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,28 +36,25 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
-    holiday_hash[:winter].values.flatten
+       holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
 holiday_hash.each do |season, holiday|
   puts "#{season.capitalize}:"
   holiday.each do |holiday_name, supplies|
-    binding.pry
-    puts "   "
+    puts "  #{holiday_name.to_s. split ("_").collect {|split_name| split_name.capitalize}.join (" ")}:
+     #{supplies.join(", ")}"
 
     end
   end
 end
-def all_holidays_with_bbq(holiday_hash)
-  # return an array of holid
 
+def all_holidays_with_bbq(holiday_hash)
+  holiday_hash.map do |season, holidays|
+    holidays.map do |holiday_name, supplies|
+      supplies.include?("BBQ") ? holiday_name : nil
+          # binding.pry
+    end
+  end
 end
