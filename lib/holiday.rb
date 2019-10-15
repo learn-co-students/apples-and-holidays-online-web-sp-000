298bc263dab2cb39631a5cad8e1b87c33199812e
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-15 01:10:11 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,8 +43,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
   puts "#{season.capitalize}:"
   holiday.each do |holiday_name, supplies|
-    puts "  #{holiday_name.to_s. split ("_").collect {|split_name| split_name.capitalize}.join (" ")}:
-     #{supplies.join(", ")}"
+    puts "  #{holiday_name.to_s.split("_").collect {|split_holiday_name| split_holiday_name.capitalize}.join(" ")}: #{supplies.join(", ")}"
 
     end
   end
@@ -56,5 +55,5 @@ def all_holidays_with_bbq(holiday_hash)
       supplies.include?("BBQ") ? holiday_name : nil
           # binding.pry
     end
-  end
+  end.flatten.compact
 end
