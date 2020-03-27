04249f2768110eab7423e895892bc6c921b22bb5
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-03-27 05:45:22 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,12 +18,23 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  # holiday_hash.each do |season, holidays|
+  #   if season == :winter
+  #     holidays.each do |holiday, supplies|
+  #       supplies << supply
+  #     end
+  #   end
+  # end
+  holiday_hash.dig(:winter).each do |holiday, supplies|
+    supplies << supply
+  end
 
 end
 
@@ -31,18 +42,18 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash.dig(:spring, :memorial_day) << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,7 +64,17 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, holidays|
+    season_text = season.to_s.capitalize + ":"
+    puts season_text
+    holidays.each do |holiday, supplies|
+      holiday_text = "  " + holiday.to_s.split("_").map(&:capitalize).join(" ") + ": "
+      supplies_text = supplies.join(", ")
+      #binding.pry
+      puts holiday_text + supplies_text
+    end
 
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
@@ -61,10 +82,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
