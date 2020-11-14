aad51cd5774d9f5943263a1579e2b86c9c2097eb
Benjamin Cheng <bencheng200@gmail.com>
2020-11-14 06:55:47 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,25 +25,45 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash.each do |season, holiday|
+    if season == :winter
+      holiday.each do |holiday, supplies|
+        supplies.push(supply)
+    end
+  end
+end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash.each do |season, holiday|
+    if season == :spring
+      holiday.each do |holiday, supplies|
+        supplies.push(supply)
+    end
+  end
+end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  arr = []
+  holiday_hash.each do |season, holiday|
+    if season == :winter
+      holiday.each do |holiday, supplies|
+        arr.push(supplies)
+    end
+  end
+end
+arr.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -54,11 +74,27 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+    holiday_hash.each do |k,v| #returning a bunch of nils with .map
+puts "#{k.capitalize}:"
+v.each do |x,y|
+title = x.to_s.tr("_", " ").split #.tr("_", " ") is new and looks like it replaces characters
+ puts "  #{title.collect {|x| x.capitalize}.join(" ")}: #{y.join(", ")}" #$$$$$$$
+ end        #The one line interpolation is really nifty awesome to see what Ruby can do.
+ end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  hello = []
+  holiday_hash.each do |z,w|
 
+      w.each do |x,y|
+   if y.include?("BBQ")
+          hello << x
+
+        end
+      end
+    end # Here it doesn't show up. The holiday_hash.each do |z,w| is still going on
+  hello
 end
