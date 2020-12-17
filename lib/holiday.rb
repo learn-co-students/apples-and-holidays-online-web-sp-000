f3af653e910034b9dcaa3202cb784b340ee0680e
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-17 18:43:00 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -7,7 +7,7 @@ def second_supply_for_fourth_of_july(holiday_supplies_hash)
       :christmas => ["Lights", "Wreath"],
       :new_years => ["Party Hats"]
     },
-    :summer => {
+    :summer => { 
       :fourth_of_july => ["Fireworks", "BBQ"]
     },
     :fall => {
@@ -17,10 +17,11 @@ def second_supply_for_fourth_of_july(holiday_supplies_hash)
       :memorial_day => ["BBQ"]
     }
   }
-  # return the second element in the 4th of July array 
+  # return the second element in the 4th of July array
 
   holiday_supplies[:summer][:fourth_of_july][1] 
 end 
+
 
 
 def add_supply_to_winter_holidays(holiday_supplies_hash, supply)
@@ -28,6 +29,7 @@ def add_supply_to_winter_holidays(holiday_supplies_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   #  AND  New Year's arrays
+ 
  holiday_supplies_hash[:winter].each do |holiday, supplies|
   supplies << supply 
 end 
@@ -36,27 +38,33 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
- 
+ holiday_hash[:spring].each do |holiday_hash, supplies|
+supplies << supply 
  end
+ end 
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash  
+  holiday_hash[:fall] = holiday_name 
+  holiday_hash[:holiday_name] =  supply_array 
   
+  
+  
+     # holiday_hash [:season][:holiday_name][:supply_array]
+   
+  # remember to return the updated hash 
   end
-
-
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 end
-
-#def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
+ 
+#def all_supplies_in_holidays(holiday_hash) 
+  # iterate through holiday_hash and print items such that your readout resembles 
+  # Winter: 
   #   Christmas: Lights, Wreath
   #   New Years: Party Hats
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 #end 
+
