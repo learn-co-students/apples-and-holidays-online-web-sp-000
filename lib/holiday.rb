8552cf9ce0ba3ca10a0f52d91f7d105ee2d49ffc
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-18 18:09:55 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,9 +59,18 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season 
   
-  holiday_hash[:season] = "winter"
-  
- 
+  holiday_supplies_hash.each do |season|
+    if season == winter
+       winter.each do |holiday, holiday_supplies|
+        holiday_supplies.each do |supply|
+         puts holiday_supplies_hash[:season][:holiday][:supply] 
+      end 
+    end 
+  end 
+end 
+end 
+      
+   
 #def all_supplies_in_holidays(holiday_hash) 
   # iterate through holiday_hash and print items such that your readout resembles 
   # Winter: 
