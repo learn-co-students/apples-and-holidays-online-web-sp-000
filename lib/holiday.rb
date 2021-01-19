e2dfe40a0e50dabaa3ea3a95efb3971c5ebe401b
Chris Baum <christopher.baum@valpo.edu>
2021-01-19 00:52:10 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,30 +18,35 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day] << supply
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
+  #binding.pry
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter].values.flatten
 
 end
 
@@ -53,18 +58,38 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, holiday|
+    puts "#{season.to_s.capitalize}:"
+    holiday.each do |event, supplies|
+      #Format season
+      format_season = event.to_s.split("_")
+      format_season = format_season.map do |x|
+        x.capitalize
+      end
+      format_season = format_season.join(" ")
 
+      #format supplies
+      format_supplies = supplies.join(", ")
+
+      #puts final format_supplies
+      puts "  #{format_season}: #{format_supplies}"
+      #binding.pry
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holiday_array = Array.new
+  holiday_hash.each do |season, holiday|
+    holiday.each do |event, supplies|
+      if supplies.any?("BBQ")
+        holiday_array << event
+      end
+    end
+  end
+  holiday_array
+  #binding.pry
 
 end
-
-
-
-
-
-
-
