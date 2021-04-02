492e91b77a2b33000bc5087f375390cf7801a422
Harrison Price <hprice518@gmail.com>
2021-04-02 23:55:13 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 
   holiday_hash[:winter].values 
-  holiday_hash[:winter].join
+  holiday_hash[:winter].to_s
 end
 
 def all_supplies_in_holidays(holiday_hash)
