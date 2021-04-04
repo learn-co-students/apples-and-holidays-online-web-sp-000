964c8549fd25f350fe4f3399ed11fcf5ad8ecc72
Harrison Price <hprice518@gmail.com>
2021-04-04 01:42:14 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,8 +72,9 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 holiday_hash.map do |season, holiday|
-  holiday.map do |name, supplies| 
-    binding.pry
+  holiday.map do |holiday_name, supplies| 
+    supplies.include? ("BBQ") ? holiday_name : nil
+    end  
   end
 end
 
