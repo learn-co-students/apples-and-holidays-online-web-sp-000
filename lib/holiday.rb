0297311dc7412c8a3ab5e9a3a3ac3b843943deb7
Harrison Price <hprice518@gmail.com>
2021-04-04 00:43:54 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |season, holiday|
+  puts "#{season.capitalize}:"
+  holiday.each do |holiday_name, supplies|
+  binding.pry
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
