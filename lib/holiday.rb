2d49e6a529ce57af532b56b9ff9fbd57d873885d
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-11 18:29:34 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,6 +66,11 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+  holiday_hash.each do |season, holiday|
+    binding.pry
+    puts season.to_s.capitalize + ": #{holiday}"
+    
+  end 
 end
 
 def all_holidays_with_bbq(holiday_hash)
